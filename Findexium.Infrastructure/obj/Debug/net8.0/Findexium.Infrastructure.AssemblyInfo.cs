a60b11cb8a5a497b946236b6b7ba5afd42d67e74
David Abruzzo <davidabruzzogames@gmail.com>
2024-10-25 12:23:55 +0200

    added empty files for rating

--- Findexium.Infrastructure/obj/Debug/net8.0/Findexium.Infrastructure.AssemblyInfo.cs
+++ Findexium.Infrastructure/obj/Debug/net8.0/Findexium.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Findexium.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64bd223652a7045049654d5c484dd43df62e1e4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88809232f433dc5c301e420662326838d0ee862")]
 [assembly: System.Reflection.AssemblyProductAttribute("Findexium.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Findexium.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
